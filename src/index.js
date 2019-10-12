e11f9a8cb545cb84768e68993a341f7b02ede8c3
JingyaoMiao <50220875+JingyaoMiao@users.noreply.github.com>
2019-10-12 21:37:22 +0800

    Test date select
    Testing the date selection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConfigProvider, DatePicker, message } from 'antd';
-// 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/es/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
+import { DatePicker, message } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
-
-moment.locale('zh-cn');
 
 class App extends React.Component {
   state = {
@@ -16,20 +10,19 @@ class App extends React.Component {
   };
 
   handleChange = date => {
-    message.info(`您选择的日期是: ${date ? date.format('YYYY-MM-DD') : '未选择'}`);
+    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
     this.setState({ date });
   };
+
   render() {
     const { date } = this.state;
     return (
-      <ConfigProvider locale={zhCN}>
-        <div style={{ width: 400, margin: '100px auto' }}>
-          <DatePicker onChange={this.handleChange} />
-          <div style={{ marginTop: 20 }}>
-            当前日期：{date ? date.format('YYYY-MM-DD') : '未选择'}
-          </div>
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={this.handleChange} />
+        <div style={{ marginTop: 20 }}>
+          Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
         </div>
-      </ConfigProvider>
+      </div>
     );
   }
 }

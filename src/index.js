b510bf70e564cd483f2d53f0995a4e6131c70cfa
JingyaoMiao <50220875+JingyaoMiao@users.noreply.github.com>
2019-10-13 15:37:01 +0800

    Login-Form

--- src/index.js
+++ src/index.js
@@ -1,40 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConfigProvider, DatePicker, message } from 'antd';
-// 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/es/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
 import './index.css';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-moment.locale('zh-cn');
-
-class App extends React.Component {
-  state = {
-    date: null,
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   };
 
-  handleChange = date => {
-    message.info(`您选择的日期是: ${date ? date.format('YYYY-MM-DD') : '未选择'}`);
-    this.setState({ date });
-  };
   render() {
-    const { date } = this.state;
+    const { getFieldDecorator } = this.props.form;
     return (
-      <ConfigProvider locale={zhCN}>
-        <div style={{ width: 400, margin: '100px auto' }}>
-          <DatePicker onChange={this.handleChange} />
-          <div style={{ marginTop: 20 }}>
-            当前日期：{date ? date.format('YYYY-MM-DD') : '未选择'}
-          </div>
-        </div>
-      </ConfigProvider>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
+ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
